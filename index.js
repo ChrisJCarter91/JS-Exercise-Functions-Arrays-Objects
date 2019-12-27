4f8286d0f8e416ad7c9f49072514692d728e11f4
chris.j.carter <chris.j.carter91@gmail.com>
2019-12-26 23:36:18 -0700

    First test pass

--- index.js
+++ index.js
@@ -39,8 +39,10 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
+function makePersonObject(id, name, email) {
+  this.id = id;
+  this.name = name;
+  this.email = email;
 }
 
 /**
